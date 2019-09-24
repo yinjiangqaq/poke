51e2cf644e87cc482bc915cf1107a617ae8412e6
ballballyoudonotstudy <1527345794@qq.com>
2019-09-24 16:28:44 +0800

    add second work

--- linjuntao/HelloWorld/ViewController.swift
+++ linjuntao/HelloWorld/ViewController.swift
@@ -10,6 +10,31 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var res: UILabel!
+    @IBOutlet weak var hello_label: UILabel!
+    
+    @IBOutlet weak var welcomeLabel: UILabel!
+    
+    @IBOutlet weak var usernameLabel: UILabel!
+    
+    @IBOutlet weak var nameText: UITextField!
+    @IBOutlet weak var passwordLabel: UILabel!
+    
+    @IBOutlet weak var passText: UITextField!
+    @IBAction func loginButtonClick(_ sender: Any) {
+        if let userName = nameText.text{
+            welcomeLabel.text = "welcome    " + userName
+        }
+        if ((nameText.text == "lin") && (passText.text == "123")) {
+            res.isHidden = false
+            res.text = "login successfully"
+        }else{
+             res.isHidden = false
+             res.text = "login failed,check your username and password!"
+            res.textColor = UIColor.red //修改字体的颜色
+            
+        }
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
