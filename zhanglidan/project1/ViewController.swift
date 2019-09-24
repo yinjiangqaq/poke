29577b9e1a8308873ba2d95d050fa90121f0549a
Apple <apple@NicdeiMac.local>
2019-09-24 17:53:36 +0800

    update

--- zhanglidan/project1/ViewController.swift
+++ zhanglidan/project1/ViewController.swift
@@ -10,11 +10,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
+   
+    @IBOutlet weak var Password: UITextField!
+    @IBOutlet weak var Account: UITextField!
+    
+    @IBOutlet weak var 登录成功: UILabel!
+    
+    @IBOutlet weak var 密码或用户名错误: UILabel!
+    
+    @IBAction func 登录(_ sender: Any) {
+        if(( Account.text == "ZLD") && ( Password.text == "000")) {
+            登录成功.isHidden = false
+        }else{
+            密码或用户名错误.isHidden=false
+        }    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-    }
-
-
-}
+        
+    }}
 
