2d4d5082f91f726c889f5128fcb80a3e375252fb
scut2017 <964372370@qq.com>
2019-10-29 17:07:37 +0800

    第四次

--- YangJieXun/SecondApp/SecondApp/FoodItemViewController.swift
+++ YangJieXun/SecondApp/SecondApp/FoodItemViewController.swift
@@ -8,21 +8,47 @@
 
 import UIKit
 
-class FoodItemViewController: UIViewController {
+class FoodItemViewController: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
 
     @IBOutlet weak var nameText: UITextField!
     @IBOutlet weak var descriptionText: UITextField!
+    
+    @IBOutlet weak var foodImage: UIImageView!
+    
     var foodForEdit: Food?
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+ 
         // Do any additional setup after loading the view.
         self.nameText.text = foodForEdit?.foodName
         self.descriptionText.text = foodForEdit?.foodDescription
         self.navigationItem.title = foodForEdit?.foodName
+        self.foodImage.image = foodForEdit?.foodAvatar
     }
 
+    @IBAction func takePhoto(_ sender: Any) {
+        let imagepicker = UIImagePickerController()
+        imagepicker.sourceType = .photoLibrary
+        imagepicker.delegate = self
+        present(imagepicker, animated: true, completion: nil)
+    }
+    
+    @IBAction func tapPhoto(_ sender: Any) {
+        let imagepicker = UIImagePickerController()
+        imagepicker.sourceType = .photoLibrary
+        imagepicker.delegate = self
+        present(imagepicker, animated: true, completion: nil)
+    }
+    
+    
+    
+    
+    func imagePickerController(_picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]){
+        let selectedImage = info[UIImagePickerController.InfoKey.originalImage] as! UIImage
+        self.foodImage.image = selectedImage
+        dismiss(animated: true, completion: nil)
+    }
     
     // MARK: - Navigation
 
@@ -32,7 +58,7 @@ class FoodItemViewController: UIViewController {
         // Pass the selected object to the new view controller.
         if segue.identifier == "saveToList"{
             print("save")
-            foodForEdit = Food(name: self.nameText.text!,description: self.descriptionText.text!)
+            foodForEdit = Food(name: self.nameText.text!,description: self.descriptionText.text!, foodAvatar: self.foodImage.image)
         }
         if segue.identifier == "cancelToList"{
             print("cancel")
