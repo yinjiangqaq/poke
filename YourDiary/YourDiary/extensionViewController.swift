e9d93eb009fc7707f089fa7bdb9bddb98ca0f8a6
zhangLIDAN <745493262@qq.com>
2019-12-10 18:03:46 +0800

    加了张力丹的界面

---
+++ YourDiary/YourDiary/extensionViewController.swift
@@ -0,0 +1,52 @@
+//
+//  extensionViewController.swift
+//  YourDiary
+//
+//  Created by Apple on 2019/12/10.
+//  Copyright © 2019 feng. All rights reserved.
+//
+
+import UIKit
+
+class extensionViewController: UIViewController {
+    
+    @IBOutlet weak var content: UITextField!
+    @IBOutlet weak var Save: UIBarButtonItem!
+    
+    var userforedit:username?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        self.content.text=userforedit?.userName
+    }
+    
+    
+    /*
+     // MARK: - Navigation
+     // In a storyboard-based application, you will often want to do a little preparation before navigation */
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+        if segue.identifier == "Save"{
+            print("save")
+            userforedit = username(userName: self.content.text)
+            
+        }
+    }
+    
+}
+
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+
---
+++ YourDiary/YourDiary/username.swift
@@ -0,0 +1,35 @@
+//
+//  username.swift
+//  YourDiary
+//
+//  Created by Apple on 2019/12/10.
+//  Copyright © 2019 feng. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class username: NSObject, NSCoding {
+    func encode(with aCoder: NSCoder) {
+        aCoder.encode(userName, forKey: "nameKey")
+        
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        userName = aDecoder.decodeObject(forKey: "nameKey") as? String
+        
+    }
+    var userName: String?
+    
+    
+    static let DocumentsDirectory = FileManager().urls(for: .documentDirectory, in: .userDomainMask).first!
+    
+    
+    
+    
+    init( userName: String?) {
+        
+        self.userName = userName
+        
+    }
+}
